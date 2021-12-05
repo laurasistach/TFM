3056b37726c28be70d60a095ede0fb69cde894c8
Laura Sistach Bosch <laurasistachbosch@gmail.com>
2021-12-05 21:46:30 +0100

    scripts for scenes 1, 2, home and calibration

---
+++ Diver_Movement.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+
+public class Diver_Movement : MonoBehaviour
+{
+    public GameObject diver_swim;
+    //public GameObject diver_breath;
+    public GameObject diver_happy;
+    public GameObject confeti;
+    public GameObject message_inhale;
+    public GameObject message_relax;
+    public GameObject message_end;
+    public GameObject mapaobj;
+    public int breathings_number;
+    public Text breathings_number_text;
+    private bool superficie;
+    private bool sorra;
+    private bool mapa;   
+    private int ValueInhale = -59; //idealment hauria d'agafar-se de Calibration.maxValueInhale
+
+    void Start()
+    {
+        //diver_breath.GetComponent<Renderer>().enabled = false;
+        //diver_swim.GetComponent<Renderer>().enabled = true;
+        message_inhale.GetComponent<Renderer>().enabled = true;
+        message_relax.GetComponent<Renderer>().enabled = false;
+        message_end.GetComponent<Renderer>().enabled = false;
+        diver_happy.GetComponent<Renderer>().enabled = false;
+        confeti.GetComponent<Renderer>().enabled = false;
+        mapaobj.GetComponent<Renderer>().enabled = true;
+
+    }
+	void OnCollisionEnter2D(Collision2D col)
+	{
+	   if (col.gameObject.name == "barra")
+	   {
+	    superficie=true;
+	    sorra = false;
+	    breathings_number++; 
+		breathings_number_text.text = breathings_number.ToString();
+	   }
+	   if (col.gameObject.name == "barra_sorra")
+	   {
+	    sorra=true;
+	    superficie = false;
+	   }
+	   if (col.gameObject.name == "map")
+	   {
+	    mapa=true;
+	   }
+	}
+
+    void Update()
+    {
+        float db = 20 * Mathf.Log10(Mathf.Abs(MicInput.MicLoudness));
+    	Vector3 position = diver_swim.transform.position;
+    	// Si encara no ha arribat a la superficie i esta respirant
+    	// agafem el 30% maximal effort
+    	if (superficie==false && db<1 && db > (ValueInhale + ValueInhale*0.3) ){
+		//if (superficie==false && Input.GetKeyDown(KeyCode.Space)){ 
+			Invoke("Swim",0);
+		}
+		if (superficie==true && sorra ==false){ //ha tocat la superficie i NO segueix respirant ////////////////////////////////
+			db = 0;
+    		Invoke("Breath",0f);
+    		Invoke("Down",2); 
+		}
+
+		if (breathings_number > 3) {
+			Vector3 position_mess = new Vector3(23, -6,0);
+			message_inhale.transform.position = position_mess;
+			message_relax.transform.position = position_mess;
+		}
+
+		if (breathings_number > 6) {
+			Vector3 position_mess2 = new Vector3(50, -6,0);
+			message_inhale.transform.position = position_mess2;
+			message_relax.transform.position = position_mess2;
+		}
+
+		if (mapa) {
+			Invoke("EndScene1",0);
+		}
+	}
+	
+	void Swim(){
+		//sorra = false;
+		//diver_swim.GetComponent<Renderer>().enabled = true;
+		//diver_breath.GetComponent<Renderer>().enabled = false;
+		Vector3 position = diver_swim.transform.position;
+		position.y = position.y + 0.1f; 
+		position.x = position.x + 0.1f; 
+		diver_swim.transform.position = position;
+	}
+
+	void Breath(){
+		message_inhale.GetComponent<Renderer>().enabled = false;
+		message_relax.GetComponent<Renderer>().enabled = true;
+		//diver_swim.GetComponent<Renderer>().enabled = false;
+		//diver_breath.GetComponent<Renderer>().enabled = true;
+		Vector3 position = diver_swim.transform.position; //obtenim posicio nedador
+		diver_swim.transform.position = position; //quedat quiet
+		//diver_breath.transform.position = position; //que el respirador tingui la posicio del nedador
+	}
+
+	void Down(){
+		//diver_breath.GetComponent<Renderer>().enabled = false; //desapareix respirador
+		//diver_swim.GetComponent<Renderer>().enabled = true; //apareix nedador
+		Vector3 position = diver_swim.transform.position;
+
+		if (sorra==false && superficie==true){ //no ha tocat la sorra pero sí la superficie
+			position.y = position.y - 0.2f; 
+			//diver_swim.transform.position = position;
+			diver_swim.transform.position = Vector3.Lerp(diver_swim.transform.position, position,Time.deltaTime * 100);
+
+		}
+		if (sorra) { // ha tocat la sorra 
+			superficie=false; // aixi s'invocarà Swim
+			message_inhale.GetComponent<Renderer>().enabled = true;
+			message_relax.GetComponent<Renderer>().enabled = false;
+		}
+
+	}
+	
+	void EndScene1(){
+		message_inhale.GetComponent<Renderer>().enabled = false;
+		mapaobj.GetComponent<Renderer>().enabled = false;
+		diver_swim.GetComponent<Renderer>().enabled = false;
+		//diver_breath.GetComponent<Renderer>().enabled = false;
+		diver_happy.GetComponent<Renderer>().enabled = true;
+		message_end.GetComponent<Renderer>().enabled = true;
+		confeti.GetComponent<Renderer>().enabled = true;
+		confeti.GetComponent<Renderer>().enabled = true;
+		//Invoke("ChangeSceneToPirates",4);
+	}
+
+	void ChangeSceneToPirates(){
+		//SceneManager.LoadScene("Scene_Pirates");  
+	}
+	
+
+}
---
+++ Veler_Movement.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+
+public class Veler_Movement : MonoBehaviour
+{
+    public GameObject prota;
+    public GameObject pirate;
+    public GameObject message_inhale;
+    public GameObject message_relax;
+    public GameObject bomba;
+    public int breathings_number;
+    public Text breathings_number_text;
+    public GameObject message_end;
+    public GameObject prota_celebrate;
+    public GameObject confeti;
+    public GameObject bruixolaObj;
+    public GameObject veler;
+    public GameObject mapa;
+    private bool end=false;
+    private bool colision = false;
+	private float timer = 3f;
+	private Vector3 posbox1;
+	private Vector3 startPosPirate;
+	public int tmax=13; 
+	public AudioSource audioSource;
+	private int ValueExhale = -36; //idealment hauria d'agafar-se de Calibration.maxValueInhale
+
+
+    void Start()
+    {
+    	prota.GetComponent<Renderer>().enabled = true;
+    	pirate.GetComponent<Renderer>().enabled = true;
+    	bruixolaObj.GetComponent<Renderer>().enabled = true;
+	    message_end.GetComponent<Renderer>().enabled = false;
+        prota_celebrate.GetComponent<Renderer>().enabled = false;
+        confeti.GetComponent<Renderer>().enabled = false;
+    }
+
+  	void OnCollisionEnter2D(Collision2D col)
+	{
+	   if (col.gameObject.name == "box1" || col.gameObject.name == "box2"|| col.gameObject.name == "box3"|| col.gameObject.name == "box4"|| col.gameObject.name == "box5"|| col.gameObject.name == "box6"|| col.gameObject.name == "box7"|| col.gameObject.name == "box8"|| col.gameObject.name == "box9")
+	   {
+	   	colision = true;
+		breathings_number++; 
+		breathings_number_text.text = breathings_number.ToString();
+		audioSource.Play();
+		Destroy(col.gameObject);
+		Debug.Log(breathings_number);
+	   }
+
+	   if (col.gameObject.name == "bruixola") 
+	   {
+	   	end=true;
+	   }
+
+	   if (col.gameObject.tag == "pirata")
+	    {
+	    	Physics2D.IgnoreCollision(col.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+	    }
+	}
+
+    void Update()
+    {
+    	float db = 20 * Mathf.Log10(Mathf.Abs(MicInput.MicLoudness));
+
+    	if (end==false){
+    	// Veler es mou sempre (lent)
+	    	Vector3 position = prota.transform.position; 
+	    	position.x = position.x + 0.01f;
+	    	prota.transform.position = position;
+
+    	}
+
+    	// Quan bufa -> es mouen més ràpid
+    	//if (end == false && Input.GetKeyDown(KeyCode.Space)){ 
+    	if (end == false && db<1 && db > (ValueExhale + ValueExhale*0.3) ){
+			Invoke("Move",0);
+		}
+
+		if (end){
+			Invoke("End",0);
+		}
+	}
+
+	void Move(){
+		Vector3 position = prota.transform.position;
+		position.x = position.x + 0.1f; //2
+		prota.transform.position = position;
+	}
+
+	void End(){
+		bruixolaObj.GetComponent<Renderer>().enabled = false;
+	   	prota.GetComponent<Renderer>().enabled = false;
+	   	veler.GetComponent<Renderer>().enabled = false;
+        mapa.GetComponent<Renderer>().enabled = false;
+    	pirate.GetComponent<Renderer>().enabled = false;
+    	bomba.GetComponent<Renderer>().enabled = false;
+	    message_end.GetComponent<Renderer>().enabled = true;
+        prota_celebrate.GetComponent<Renderer>().enabled = true;
+        confeti.GetComponent<Renderer>().enabled = true;
+	}
+		
+	void ChangeSceneToPirates(){
+		SceneManager.LoadScene("Scene_MountainsFlying");  
+	}
+	
+
+}
