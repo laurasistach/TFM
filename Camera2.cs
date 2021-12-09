e12edcf669fdbac0873dd9e5332503d7ce3aff98
Laura Sistach Bosch <laurasistachbosch@gmail.com>
2021-12-09 20:12:16 +0100

    deleted score elements in camera script

--- Camera2.cs
+++ Camera2.cs
@@ -4,19 +4,15 @@ using UnityEngine;
 
 public class Camera2 : MonoBehaviour
 {
-	public Transform Followplatform;
+	public Transform Camera;
     public Transform Player;
-    public Transform Score; 
  
     void LateUpdate ()
     {
     	// Camera and player positioning
-        Vector3 cam = Followplatform.position = new Vector3(Player.position.x + 10, 5, -9.7f);
-        Vector3 vec1 = Followplatform.position;
-        Followplatform.position = Vector3.Lerp(vec1, cam, 10);
-
-        // Score square position
-        Vector3 score_pos = Score.position = new Vector3(Player.position.x + 40,17,0);
+        Vector3 cam = Camera.position = new Vector3(Player.position.x + 10, 5, -9.7f);
+        Vector3 vec1 = Camera.position;
+        Camera.position = Vector3.Lerp(vec1, cam, 10);
 
 	}
 }
--- Camera4.cs
+++ Camera4.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera3 : MonoBehaviour
+public class Camera4 : MonoBehaviour
 {
 	public Transform Camera;
     public Transform Player;
@@ -10,7 +10,7 @@ public class Camera3 : MonoBehaviour
     void LateUpdate ()
     {
     	// Camera and player positioning
-        Vector3 cam = Camera.position = new Vector3(Player.position.x, -3, -10f);
+        Vector3 cam = Camera.position = new Vector3(Player.position.x, 0, -10f);
         Vector3 vec1 = Camera.position;
         Camera.position = Vector3.Lerp(vec1, cam, 10);
 
