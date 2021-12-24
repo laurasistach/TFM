b8014ad57728e50bfe803567fd011e28566761e6
Laura Sistach Bosch <laurasistachbosch@gmail.com>
2021-12-24 19:19:26 +0100

    scripts to show instructions before each scene

---
+++ Instruct1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;  
+
+public class Instruct1: MonoBehaviour {  
+	public Button button1;
+	public GameObject intruction1;
+	public GameObject intruction2;
+	private string currentOption = "option 1";
+	
+	void Start() {  
+		intruction1.SetActive(true);
+        intruction2.SetActive(false);
+        Button btn1 = button1.GetComponent<Button>();
+        btn1.onClick.AddListener(myButtonClick);
+    }  
+
+    void myButtonClick()
+     {
+        switch (currentOption)
+        {
+            case "option 1":
+                intruction2.SetActive(true);
+        		intruction1.SetActive(false);
+                currentOption = "option 2";
+                break;
+ 
+             case "option 2":
+                SceneManager.LoadScene("Scene_UnderTheSea");  
+                break;
+        }
+    }
+}   
---
+++ Instruct3.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;  
+
+public class Instruct3: MonoBehaviour {  
+	public Button button1;
+	public GameObject intruction1;
+	public GameObject intruction2;
+	private string currentOption = "SEGÜENT";
+	
+	void Start() {  
+		intruction1.SetActive(true);
+        intruction2.SetActive(false);
+        Button btn1 = button1.GetComponent<Button>();
+        btn1.onClick.AddListener(myButtonClick);
+        setButtonText(currentOption);
+    }  
+
+    void myButtonClick()
+    {
+        switch (currentOption)
+        {
+            case "SEGÜENT":
+                intruction2.SetActive(true);
+        		intruction1.SetActive(false);
+                currentOption = "COMENÇAR!";
+                setButtonText(currentOption);
+                break;
+ 
+            case "COMENÇAR!":
+                SceneManager.LoadScene("Scene_MountainsFlying");  
+                break;
+        }
+    }
+    void setButtonText(string buttonText)
+    {
+        button1.transform.GetChild(0).GetComponent<Text>().text = buttonText;
+    }
+}   
