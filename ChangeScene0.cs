39ab91025a60e60de6ff452c4973f80cd57700fb
Laura Sistach Bosch <laurasistachbosch@gmail.com>
2021-12-24 19:18:30 +0100

    go to AllScenes

---
+++ ChangeScene0.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;  
+
+public class ChangeScene0: MonoBehaviour {  
+	public Button buttonGoScene;
+	
+	void Start() {  
+
+        Button btn = buttonGoScene.GetComponent<Button>();
+        btn.onClick.AddListener(Scene1);
+    }  
+
+    void Scene1(){
+    	SceneManager.LoadScene("AllScenes");  
+    }
+
+}   
