711a2044c3f7a4735e257c9a2f396aab223f40b9
Laura Sistach Bosch <laurasistachbosch@gmail.com>
2021-12-24 19:21:56 +0100

    go to instructions screens

---
+++ ChangeScene2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;  
+
+public class ChangeScene2: MonoBehaviour {  
+	public Button buttonGoScene;
+	
+	void Start() {  
+
+        Button btn = buttonGoScene.GetComponent<Button>();
+        btn.onClick.AddListener(Scene3);
+    }  
+
+    void Scene3(){
+    	SceneManager.LoadScene("Instructions3");  
+    }
+
+}   
